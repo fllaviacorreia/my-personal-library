a58b09d47234ddf254a2e2c3d4a578b4b37faede
Flávia Correia <fllaviacorreia@outlook.com>
2025-06-09 20:52:06 -0300

    create initial screens

---
+++ src/app/add.tsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function Add()
+{
+    return (
+        <View>
+            <Text> Add Screen</Text>
+
+                        <Link href="/home">
+                            <Text>Ir para Home</Text>
+                        </Link>
+        </View>
+    )
+}
