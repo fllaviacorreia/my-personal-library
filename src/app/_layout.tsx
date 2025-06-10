9af9123c32d14e23c679e7eb0f3089b117087cab
Flávia Correia <fllaviacorreia@outlook.com>
2025-06-09 21:34:13 -0300

    englobe context in route file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,20 @@
-import {Stack} from 'expo-router'
+import { BooksProvider } from '@/context/booksContext'
+import { Stack } from 'expo-router'
 
-export default function Layout(){
+export default function Layout() {
     return (
-        <Stack initialRouteName='home'>
-            <Stack.Screen 
-                name='home' 
-                options={{ title:"Estante Virtual", headerTitleAlign: 'center'}}
-            />
-            <Stack.Screen 
-                name='add' 
-                options={{ title:"Novo Livro", headerTitleAlign: 'center'}}
-            />            
-        </Stack>
+        <BooksProvider>
+            <Stack initialRouteName='home'>
+                <Stack.Screen
+                    name='home'
+                    options={{ title: "Estante Virtual", headerTitleAlign: 'center' }}
+                />
+                <Stack.Screen
+                    name='add'
+                    options={{ title: "Novo Livro", headerTitleAlign: 'center' }}
+                />
+            </Stack>
+        </BooksProvider>
+
     )
 }
