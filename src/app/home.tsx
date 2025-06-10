0234bbe3028b1664c2e7ded1199895075e7021b6
Flávia Correia <fllaviacorreia@outlook.com>
2025-06-09 22:04:54 -0300

    call components in home

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,10 +1,15 @@
+import Button from "@/components/button";
+import Input from "@/components/input";
 import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function Home() {
     return (
-        <View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 25}}>
             <Text> Home Screen</Text>
+            <Input placeholder="Pesquise por título" />
+            <Button label="Adicionar livro" size="medium" status="primary" />
+
             <Link href="/add">
                 <Text>Ir para ADD</Text>
             </Link>
