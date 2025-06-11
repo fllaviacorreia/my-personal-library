9070354ffdf3f563bdccec5a7f2008b3a070ec19
Flávia Correia <fllaviacorreia@outlook.com>
2025-06-11 15:03:18 -0300

    finish style and rules of home

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,25 +1,65 @@
 import Button from "@/components/button";
 import Card from "@/components/card";
 import Input from "@/components/input";
-import { BookType } from "@/context/booksContext";
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { useBooks } from "@/context/booksContext";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { View, Text, SectionList, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
-    const book1: BookType = {id: "adv12", title:"Titulo 1", authors: "Autores 1", status: "reading"}
-    const book2: BookType = {id: "adv22", title:"Titulo 2", authors: "Autores 2", status: "to-read"}
-    const book3: BookType = {id: "adv32", title:"Titulo 2", authors: "Autores 2", status: "reading"}
-    
+    const router = useRouter();
+
+    const {books, getBooksBySection} = useBooks();
+
+    const [titleOfBook, setTitleOfBook] = useState("");
+
+    const filteredBooks = books.filter(book => book.title.toLowerCase().includes(titleOfBook.toLocaleLowerCase()))
+
+    const sections = getBooksBySection(filteredBooks);
+
+    const handleGoAddBookScreen = () => {
+        router.push("/add")
+    }
+   
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 25}}>
-            <Text> Home Screen</Text>
-            <Input placeholder="Pesquise por título" />
-            <Button label="Adicionar livro" size="medium" status="outline" />
-            <Card {...book1}/>
-            <Card {...book2}/>
-            <Link href="/add">
-                <Text>Ir para ADD</Text>
-            </Link>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.search}>
+                <Input 
+                    placeholder="Pesquise por título" 
+                    value={titleOfBook} 
+                    onChangeText={(value) => setTitleOfBook(value)}/>
+            </View>
+            <View style={styles.listContainer}>
+                <Text style={styles.titleScreen}>Meus Livros</Text>
+                <SectionList
+                    sections={sections}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({item}) => <Card {...item} />}
+                    ListEmptyComponent={<Text style={{color: "orange"}}>Nenhum livro encontrado</Text>}
+                    renderSectionHeader={({section:  {title}}) => (<Text style={styles.titleSection}>{title}</Text>)}
+                />
+            </View>
+            <View style={styles.footerContainer}>
+            <Button label="Adicionar livro" size="medium" status="primary" onPress={() => handleGoAddBookScreen()}/>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        alignItems: 'flex-start',         
+        paddingHorizontal: 20, 
+        gap: 25, 
+        paddingVertical: 15, 
+        backgroundColor: '#F0FDEC'
+    },
+    search: {width: "100%", marginVertical: 10},
+    listContainer: {height: "80%", gap: 25, width: "100%",},
+    footerContainer: {width: "100%", alignItems: 'center'},
+    titleScreen:{fontSize: 20, fontWeight: '900'},
+    titleSection: {fontSize: 16, fontWeight: "bold"}
+
+})
